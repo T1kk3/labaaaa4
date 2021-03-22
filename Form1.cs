57e6c2c2a6d02bdda1c8c9015efa500c7f7d0f9a
tikke tikke <tikke.flex@yandex.ru>
2021-03-22 10:27:04 +0300

    123

---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace labaaaa4
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+      textBox2.Text = "Результат: ";
+    }
+
+    public static double factorial(double n)
+    {
+      int r = 1;
+      for (int i = 2; r <= n; ++i)
+        r *= i;
+      return r;
+    }
+
+      public double CalcSum(double n)
+      {
+        double sum = 0;
+        for (int i = 1; i <= n; i++)
+        {
+        sum += (5 + n) / (factorial(3 * n) - 1);
+        }
+
+      textBox2.Text += Environment.NewLine +
+      "Результат: " + sum.ToString();
+      return sum;
+      }
+
+    public double CalcPow(double n)
+    {
+      double sumpow = 0;
+      for (int i = 1; i <= n; i++)
+      {
+        sumpow += 0.5 * (2 + factorial(0.1));
+      }
+
+      textBox2.Text += Environment.NewLine +
+      "Результат: " + sumpow.ToString();
+      return sumpow;
+    }
+    private void textBox2_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void textBox1_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    public void button1_Click(object sender, EventArgs e)
+    {
+
+      double n = double.Parse(textBox1.Text);
+      textBox2.Text += Environment.NewLine +
+      "При n = " + n.ToString();
+      int x;
+      if (radioButton1.Checked) x = 1; else x = 2;
+
+      switch (x)
+      {
+        case 1:
+          CalcSum(n);
+          break;
+
+        case 2:
+          CalcPow(n);
+          break;
+
+        default:
+          break;
+      }
+    }
+
+    private void radioButton1_CheckedChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void radioButton2_CheckedChanged(object sender, EventArgs e)
+    {
+
+    }
+  }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("labaaaa4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("labaaaa4")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("ad8018e1-84f5-43a5-98a1-0895a7f24a1d")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
